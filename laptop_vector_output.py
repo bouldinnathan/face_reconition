b1f399bad6364ee0a6508e785a3c484d7fb2e0f5
bouldinnathan <45728244+bouldinnathan@users.noreply.github.com>
2019-05-17 23:24:51 -0500

    initial commit

---
+++ laptop_vector_output.py
@@ -0,0 +1,318 @@
+import tensorflow as tf
+from tensorflow.python.keras.datasets import cifar10
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Dropout, Activation, Flatten, Reshape
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose,BatchNormalization
+from tensorflow.python.keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+#from tensorflow.python.keras.backend.tensorflow_backend import set_session
+import matplotlib.pyplot as plt
+from tensorflow.python.keras import backend
+
+import threading
+from multiprocessing import Pipe
+
+import pickle
+import os
+import cv2
+import numpy as np
+global make_image_count
+make_image_count=0
+
+
+add_train=False
+count=15
+
+def make_directory_if_not_exists(path):
+    while not os.path.isdir(path):
+        try:
+            os.makedirs(path)
+            break    
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+        except WindowsError:
+            print ("got WindowsError")
+            pass  
+
+def make_image(X):
+    global make_image_count
+    make_image_count=make_image_count+1
+    data=X
+    prediction=model.predict(data, batch_size=None, verbose=1)
+    print(prediction[1])
+    prediction*= 255.
+    prediction=prediction.astype(np.uint8)
+
+    for i in range(len(prediction[1])):
+        prediction[i]=cv2.cvtColor(prediction[i],cv2.COLOR_BGR2RGB)
+    #print(Prediction[11])
+
+    #plt.imshow(Prediction[11])                           
+    #plt.axis('off')
+    #plt.show()
+    print(prediction[1])
+    print(prediction.shape)
+
+    #show the two pictures
+    vis = np.concatenate((X, prediction), axis=1)
+    cv2.imwrite(str(make_image_count)+'.jpg', vis)
+    
+def resize_pic(child_conn, img_local):
+    img = cv2.imread(img_local)
+    img=cv2.resize(img,(100,100),cv2.INTER_CUBIC)
+    child_conn.send(img)
+#from keras import backend as K
+#config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.9
+#config.gpu_options.allow_growth=True
+
+#K.clear_session()
+#sess = tf.Session(config=config)
+#K.set_session(sess)
+###################################
+#config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.5
+#config.gpu_options.allow_growth=True
+#sess = tf.Session(config=config)
+
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'#no gpu
+###################################
+
+
+#class MemoryCallback(Callback):
+#    def on_epoch_end(self, epoch, log={}):
+#        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
+
+src = "./data" #pokeRGB_black
+dst = "./img_align_celeba" # resized
+dst2= "./crop_faces_dudes"
+#dst3= "./crop_not_face"
+
+X=[]
+Y=[]
+not_face=[]
+parent_conn=list()
+child_conn=list()
+d=list()
+IMG_SIZE=50
+IMG_SIZE_UP=IMG_SIZE*2
+img_count=0
+true_count=0
+
+for each in os.listdir(dst):
+    temp, temp2 = Pipe()
+    parent_conn.append(temp)
+    child_conn.append(temp2)
+    
+    if (not img_count==0):
+        img=parent_conn[img_count-1].recv()
+        parent_conn[img_count-1].close()
+        d[img_count-1].join()
+        X.append(img)
+        if img_count%300==0:
+            print('Images proccessed: '+str(img_count))
+            
+    if (not img_count==0)and img_count%300==0:
+        print('Images loading: '+str(len(X)))
+        
+    #img = cv2.imread(os.path.join(dst,each))
+            
+
+    d.append( threading.Thread(name='daemon', target=resize_pic, args=(child_conn[img_count], os.path.join(dst,each))))
+    d[img_count].setDaemon(True)
+    d[img_count].start()
+    img_count=img_count+1
+
+img=parent_conn[img_count-1].recv()
+parent_conn[img_count-1].close()
+d[img_count-1].join()
+X.append(img)
+
+del parent_conn,d
+####for i in range(img_count):
+####
+####    img=parent_conn[i].recv()
+####    parent_conn[i].close()
+####    d[i].join()
+####    X.append(img)
+####    if i%500==0:
+####        print('Images proccessed: '+str(i))
+
+##for each in os.listdir(dst2):
+##    
+##    img = cv2.imread(os.path.join(dst2,each))
+##    img=cv2.resize(img,(100,100),cv2.INTER_CUBIC)
+##    
+##    X.append(img)
+
+##for each in os.listdir(dst3):
+##    
+##    img = cv2.imread(os.path.join(dst,each))
+##    img=cv2.resize(img,(100,100),cv2.INTER_CUBIC)
+##    
+##    not_face.append(img)
+
+print(X[0].shape)
+X = np.array(X).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)#type changed to numpy for shape
+#Y = np.array(Y).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)#type changed to numpy for shape
+#not_face = np.array(Y).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)#type changed to numpy for shape
+X=np.true_divide(X, 255.0)
+#X = X/255.0
+#Y = Y/255.0
+#not_face = not_face/255.0
+
+#pickle_in = open("X.pickle","rb")
+#X = pickle.load(pickle_in)
+
+#pickle_in = open("y.pickle","rb")
+#y = pickle.load(pickle_in)
+
+#X = X/255.0
+
+if add_train:
+    current_output_local='output/million_a'+str(count)+'/'
+    model = load_model(current_output_local+'/my_model.h5')
+    print(model.summary())
+else:
+    model = Sequential()
+
+    #model.add(Conv2D(100, (3, 3), input_shape=X.shape[1:]))
+    #model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Conv2D(100, (3, 3)))
+    #model.add(Activation('relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    #model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+    #model.add(Dense(64))
+
+    #model.add(Dense(1))
+    #model.add(Activation('sigmoid'))
+
+    print(X.shape)    
+
+    model.add(Conv2D(128, (2, 2), input_shape=X.shape[1:],padding="SAME"))#input_shape=X.shape[1:]))
+    #model.add(Conv2D(128, (2, 2),padding="SAME"))
+    model.add(Conv2D(1, (2, 2),padding="SAME"))
+    model.add(Flatten())
+    model.add(Dense(1000))
+    model.add(Dense(10000))
+    model.add(Reshape((100, 100,1)))
+    model.add(Conv2D(1, (2, 2),padding="SAME"))
+    #model.add(Conv2D(32, (2, 2),padding="SAME"))
+    model.add(Conv2D(128, (2, 2),padding="SAME"))
+    model.add(Conv2D(3, (2, 2),padding="SAME"))
+
+
+
+    model.add(Activation('tanh'))
+    print('finished model')
+
+    model.compile(loss='mse',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    print(model.summary())
+
+
+
+
+temp=X[0:100,:,:,:]
+
+count=count+1
+
+current_output_local='output/million_a'+str(count)+'/'
+make_directory_if_not_exists(current_output_local)
+
+filepath=current_output_local+"weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+checkpoint_two=ModelCheckpoint(current_output_local+'please.ckpt', save_weights_only=True, verbose=1)
+callbacks_list = [checkpoint,checkpoint_two]
+
+cv2.imwrite(current_output_local+'id10tCheck'+".jpg", X[2])
+
+model.fit(X, X, batch_size=50, epochs=5, validation_split=0.3,callbacks=callbacks_list,verbose=1,shuffle=True)
+
+model.save(current_output_local+'/my_checkpoint_test', overwrite=True)  # creates a HDF5 file 'my_model.h5'
+#del model  # deletes the existing model
+#model = load_model('output/my_model.h5')
+
+######## save method two https://jovianlin.io/saving-loading-keras-models/
+# Save the weights
+model.save_weights(current_output_local+'model_weights.h5', overwrite=True)
+
+# Save the model architecture
+with open(current_output_local+'model_architecture.json', 'w') as f:
+    f.write(model.to_json())
+#######
+############save method three#################
+saver = tf.train.Saver()
+sess = backend.get_session()
+saver.save(sess, current_output_local)
+
+model.save(current_output_local+'my_model.h5')
+#print(model.get_weights())
+
+
+############    
+###does not like to reopen old
+##    del model
+##    del saver
+##
+##    model = load_model('keras_model.hdf5')
+##
+##    saver = tf.train.Saver()
+##    sess = backend.get_session()
+##    saver.restore(sess, current_output_local)
+
+
+
+
+
+###########################perdiction####################################
+print(X.shape)
+#temp=temp
+data=X[0:100,:,:,:]
+del X
+data=np.array(data).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)
+#print(data)
+prediction=model.predict(data, batch_size=None, verbose=1)
+print(prediction[1])
+
+prediction*= 255.
+prediction=prediction.astype(np.uint8)
+
+data*= 255
+data=data.astype(np.uint8)
+
+#temp*= 255
+#temp=temp.astype(np.uint8)
+#for i in range(len(prediction[1])):
+#    prediction[i]=cv2.cvtColor(prediction[i],cv2.COLOR_BGR2RGB)
+#print(Prediction[11])
+
+#plt.imshow(Prediction[11])                           
+#plt.axis('off')
+#plt.show()
+print(prediction[1])
+print(prediction.shape)
+
+#show the two pictures
+vis = np.concatenate((data, prediction), axis=1)
+del data,prediction
+#cv2.imshow("Data vs Prediction",Y[3]);
+#cv2.waitKey(0);
+filename=0
+print(vis.shape)
+for pairs in vis:
+    filename=filename+1
+    cv2.imwrite(current_output_local+str(filename)+".jpg", pairs)
+    #plt.imshow(pairs)                           
+    #plt.axis('off')
+    #plt.show()
+
+
+
---
+++ predict_model_picture_import.py
@@ -0,0 +1,341 @@
+import tensorflow as tf
+from tensorflow.python.keras.datasets import cifar10
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose,BatchNormalization
+from tensorflow.python.keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+#from tensorflow.python.keras.backend.tensorflow_backend import set_session
+import matplotlib.pyplot as plt
+from tensorflow.python.keras import backend
+
+
+
+
+from tensorflow.python.keras.models import load_model
+from keras.models import Model
+from keras.layers import Input
+#from tensorflow.python.keras.models import predict
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import model_from_json
+import tensorflow as tf
+from tensorflow.python.keras import backend
+
+from itertools import chain
+
+sensitivity=1.1
+##########################################,name='midPoint'
+
+def make_directory_if_not_exists(path):
+    while not os.path.isdir(path):
+        try:
+            os.makedirs(path)
+            break    
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+        except WindowsError:
+            print ("got WindowsError")
+            pass  
+
+
+
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'#no gpu
+
+src = "./data" #pokeRGB_black
+dst = "./crop_face1_female" # resized
+dst2= "./crop_face1_male"
+
+
+X=[]
+Y=[]
+IMG_SIZE=50
+IMG_SIZE_UP=IMG_SIZE*2
+
+for each in os.listdir(dst):
+    
+    img = cv2.imread(os.path.join(dst,each))
+    img=cv2.resize(img,(100,100),cv2.INTER_CUBIC)
+    X.append(img)
+    person1=X
+print(len(X))
+
+for each in os.listdir(dst2):
+    
+    img = cv2.imread(os.path.join(dst2,each))
+    img=cv2.resize(img,(100,100),cv2.INTER_CUBIC)
+    Y.append(img)
+print(len(Y))
+person2=Y
+person2AndPerson1=list(person2)
+print('length of person2 person2AndPerson1 '+str(len(person2AndPerson1)))
+#person2AndPerson1.append(person2)
+person2AndPerson1.extend(person1)
+print('length of person2+person1 person2AndPerson1 '+str(len(person2AndPerson1)))
+
+#interleving to face to make faults matches
+interlevedPerson2AndPerson1=list(chain.from_iterable(zip(person1[:len(person2)], person2[:len(person1)])))
+print('Length of interleved '+str(len(interlevedPerson2AndPerson1)))
+
+notMatched=[]
+for i in range(len(interlevedPerson2AndPerson1)):
+    notMatched.append([0,1])
+notMatched = np.array(notMatched).reshape(-1,2)
+
+print(type(person2AndPerson1))
+person2AndPerson1.extend(interlevedPerson2AndPerson1)
+X=person2AndPerson1
+print('length of person2+person1+interleaved person2AndPerson1 '+str(len(person2AndPerson1)))
+
+
+
+answers=np.array([[1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [0,1],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0],
+         [1,0]])
+
+answers=np.concatenate((answers, notMatched), axis=0)
+
+
+X = np.array(X).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)#type changed to numpy for shape
+#Y = np.array(Y).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)#type changed to numpy for shape
+X = X/255.0
+#Y = Y/255.0
+answers = np.array(answers).reshape(-1,2)#type changed to numpy for shape
+
+#plt.imshow(X[11])                           
+#plt.axis('off')
+#plt.show()
+
+current_output_local='output/a1/'
+
+model = load_model(current_output_local+'weights-improvement-02-0.89.hdf5')#'my_model.h5'
+model.summary()
+####
+####
+#####model = model_from_json(current_output_local+'model_architecture.json')
+####model.load_weights(current_output_local+'model_weights.h5')
+##
+####def loadModel(jsonStr, weightStr):
+####    json_file = open(jsonStr, 'r')
+####    loaded_nnet = json_file.read()
+####    json_file.close()
+####
+####    serve_model = model_from_json(loaded_nnet)
+####    serve_model.load_weights(weightStr)
+####
+####    serve_model.compile(optimizer=tf.train.AdamOptimizer(),
+####                        loss='mse',
+####                        metrics=['accuracy'])
+####    return serve_model
+#
+
+#print(model.get_weights())
+#model = loadModel(current_output_local+'model_architecture.json', current_output_local+'model_weights.h5')#, current_output_local+'model_weights.h5'
+
+#model = model.load_weights(current_output_local+'weights-improvement-01-0.83.hdf5')
+
+
+
+#model = load_model(current_output_local+'keras_model.hdf5')
+
+
+
+##model = load_model(current_output_local+'please.ckpt.index')
+##
+##saver = tf.train.Saver()
+##sess = backend.get_session()
+##saver.restore(sess, current_output_local)
+
+#model.load_weights(current_output_local+'please.ckpt')
+
+#model.set_weights(current_output_local+'please.ckpt')#'please.ckpt'
+
+
+###########################perdiction####################################
+#print(Y.shape)
+#X=X[0:100,:,:,:]
+#data=X[0:100,:,:,:]
+#print(data.shape)
+#data=np.array(data).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 3)
+#print(data.shape)
+#print(data)
+
+
+
+
+inp = model.input                                           # input placeholder
+outputs = [layer.output for layer in model.layers]          # all layer outputs
+functors = [backend.function([inp, backend.learning_phase()], [out]) for out in outputs]    # evaluation functions
+
+test=X
+layer_outs = [func([test, 1.]) for func in functors]
+
+
+
+
+#prediction=model.predict(data, batch_size=None, verbose=1)
+
+
+prediction=layer_outs
+
+#print(prediction[1])
+
+#prediction*= 255
+
+#print(prediction[3][0])
+
+data=prediction[2]
+#data=prediction[5][0][0]
+data = np.array(data)
+data*= 255
+data=data.astype(np.uint8)
+data=data[0]
+
+
+#prediction=int(prediction)
+#prediction*= 255
+#prediction=prediction.astype(np.uint8)
+print(data.shape)
+mid_point_data=list()
+for i in range(len(data)-1):
+    if i<0:
+        1+1
+        print(i)
+    else:
+        #mid_point_data=[mid_point_data,np.concatenate((data[i],data[i+1]),axis=1)]
+        mid_point_data.append(np.concatenate((data[i],data[i+1]),axis=-1))
+        #mid_point_data=np.concatenate(mid_point_data,np.concatenate(data[0][i],data[0][i+1],axis=1),axiz=0)
+        print(i)
+mid_point_data=np.asarray(mid_point_data)
+print('shape of data: '+str(mid_point_data.shape))
+
+mid_point_data = np.array(mid_point_data).reshape(-1, IMG_SIZE_UP, IMG_SIZE_UP, 2)
+model2 = Sequential()
+#print(X.shape)    
+model2.add(Conv2D(2, (2, 2), input_shape=mid_point_data.shape[1:],padding="SAME",name='inputPoint'))#input_shape=X.shape[1:]))
+model2.add(Conv2D(128, (2, 2),padding="SAME"))
+model2.add(Flatten())
+model2.add(Dense(2, activation='relu'))
+model2.compile(loss='mse',
+                optimizer='adam',
+                metrics=['accuracy'])
+print(model2.summary())
+
+model2.fit(mid_point_data, answers, batch_size=102, epochs=1, validation_split=0.3,verbose=1,shuffle=True)
+##############print(data1)
+##############
+##############x,y,z=data1.shape
+##############nzCount = np.count_nonzero(data1)
+##############print(str(data1.shape)+" has number of non-zeros: "+str(nzCount)+' of '+str(x*y*z)+' and zeros: '+str(x*y*z-nzCount))
+##############original=x*y*z-nzCount
+##############
+##############subtract_data=np.subtract(data2,data1*sensitivity)
+##############x,y,z=subtract_data.shape
+##############nzCount = np.count_nonzero(subtract_data>0)
+##############print(str(subtract_data.shape)+" has number of non-zeros: "+str(nzCount)+' of '+str(x*y*z)+' and zeros: '+str(x*y*z-nzCount))
+##############original_max=x*y*z-nzCount
+##############
+##############subtract_data=np.add(data2,data1*-(sensitivity-2))
+##############x,y,z=subtract_data.shape
+##############nzCount = np.count_nonzero(subtract_data>0)
+##############print(str(subtract_data.shape)+" has number of non-zeros: "+str(nzCount)+' of '+str(x*y*z)+' and zeros: '+str(x*y*z-nzCount))
+##############original_min=x*y*z-nzCount
+##############
+##############match=0
+##############
+##############if original<original_max and original>original_min:
+##############    match=match+1
+#############################################################
+
+
+
+
+
+
+#for i in range(len(prediction[1])):
+#    prediction[i]=cv2.cvtColor(prediction[i],cv2.COLOR_BGR2RGB)
+#print(Prediction[11])
+
+#plt.imshow(Prediction[11])                           
+#plt.axis('off')
+#plt.show()
+
+
+#show the two pictures
+##############vis = np.concatenate((Y,data, prediction), axis=1)
+#cv2.imshow("Data vs Prediction",vis);
+#cv2.waitKey(0);
+############filename=0
+############print(vis.shape)
+############for pairs in vis:
+############    filename=filename+1
+############    cv2.imwrite(current_output_local+str(filename)+".jpg", pairs)
+    #plt.imshow(pairs)                           
+    #plt.axis('off')
+    #plt.show()
+cv2.imwrite(str(1)+".jpg", np.concatenate((prediction[5][0][0]*255,prediction[5][0][1]*255,prediction[5][0][2]*255,prediction[5][0][3]*255), axis=1))
